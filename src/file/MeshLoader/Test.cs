75e1e3937263354edc9a092901c3baf9aa55f25c
failrate <failrate@gmail.com>
2014-03-08 18:06:16 -0800

    MeshLoader Initial Checkin
    Creating the meshloader project

---
+++ src/file/MeshLoader/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeshLoader
+{
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            // \todo: instantiate a MeshLoader
+            // \todo: load the test file into a mesh data structure
+            // \todo: validate that the test mesh data structure matches the expected value
+        }
+    }
+}
