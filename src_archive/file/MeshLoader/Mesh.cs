da4a7f92015aad795cecc8c56af7ac2e3d3769e9
Andrew Baker <failrate@gmail.com>
2014-05-02 19:42:10 -0700

    modified:   file/MeshLoader/Mesh.cs 	modified:   file/MeshLoader/MeshLoader.cs 	modified:   file/MeshLoader/MeshLoader.csproj 	modified:   file/MeshLoader/Test.cs 	file/MeshLoader/ChopperCamera.cs 	file/MeshLoader/MeshRender.cs

--- src_archive/file/MeshLoader/Mesh.cs
+++ src_archive/file/MeshLoader/Mesh.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using OpenTK;
+
 namespace MeshLoader
 {
     public class ChopperFace
@@ -23,6 +24,24 @@ namespace MeshLoader
             textureCoordinates = new List<Vector3>();
             normalCoordinates = new List<Vector3>();
         }
+		public List<int> facesAsTriangles()
+		{
+			List<int> triangles = new List<int>();
+				foreach (ChopperFace face in faces)
+				{
+					triangles.Add(face.vertexIndices[0] - 1);
+					triangles.Add(face.vertexIndices[1] - 1);
+					triangles.Add(face.vertexIndices[2] - 1);
+					if (face.vertexIndices.Count > 3)
+					{
+						triangles.Add(face.vertexIndices[2] - 1);
+						triangles.Add(face.vertexIndices[3] - 1);
+						triangles.Add(face.vertexIndices[0] - 1);
+					}
+				}
+
+			return triangles;
+		}
     }
 	public class ChopperObject
     {
@@ -47,6 +66,33 @@ namespace MeshLoader
         {
             currentGroup.normalCoordinates.Add(coord);
         }
+		public void ensureCurrentGroup()
+		{
+			if (currentGroup == null)
+			{
+				addGroup("default");
+			}
+		}
+		public List<int> facesAsTriangles()
+		{
+			List<int> triangles = new List<int>();
+			foreach (ChopperGroup group in groups)
+			{
+				foreach (ChopperFace face in group.faces)
+				{
+					triangles.Add(face.vertexIndices[0] - 1);
+					triangles.Add(face.vertexIndices[1] - 1);
+					triangles.Add(face.vertexIndices[2] - 1);
+					if (face.vertexIndices.Count > 3)
+					{
+						triangles.Add(face.vertexIndices[2] - 1);
+						triangles.Add(face.vertexIndices[3] - 1);
+						triangles.Add(face.vertexIndices[0] - 1);
+					}
+				}
+			}
+			return triangles;
+		}
         public void addFace(List<int> vertexIndices, List<int> textureIndices, List<int> normalIndices, bool useSmoothing, string materialName)
         {
             ChopperFace face = new ChopperFace();
@@ -55,28 +101,9 @@ namespace MeshLoader
             face.normalIndices = normalIndices;
             face.useSmoothing = useSmoothing;
             face.materialName = materialName;
+			ensureCurrentGroup();
             currentGroup.faces.Add(face);
         }
-		public List<int> facesAsTriangles()
-		{
-			List<int> triangles = new List<int>();
-            foreach (ChopperGroup group in groups)
-            {
-                foreach (ChopperFace face in group.faces)
-                {
-                    triangles.Add(face.vertexIndices[0] - 1);
-                    triangles.Add(face.vertexIndices[1] - 1);
-                    triangles.Add(face.vertexIndices[2] - 1);
-                    if (face.vertexIndices.Count > 3)
-                    {
-                        triangles.Add(face.vertexIndices[2] - 1);
-                        triangles.Add(face.vertexIndices[3] - 1);
-                        triangles.Add(face.vertexIndices[0] - 1);
-                    }
-                }
-            }
-			return triangles;
-		}
 		public List<Vector2> uvs()
 		{
 			List<Vector2> uvs = new List<Vector2>();
